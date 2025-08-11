7d050fb3878433796c318b0f3e20bc07f3aef87a
gifary10 <57998309+gifary10@users.noreply.github.com>
2025-08-11 11:57:06 +0700

    Add files via upload

---
+++ js/services/state-service.js
@@ -0,0 +1,71 @@
+// state-service.js
+const DEFAULT_STATE = {
+    currentClause: null,
+    currentSubclause: null,
+    searchTerm: '',
+    clausesData: {},
+    lastFetchTime: {},
+    progress: {},
+    activeTab: 'implementation'
+};
+
+let appState = deepClone(DEFAULT_STATE);
+
+function deepClone(obj) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    
+    if (Array.isArray(obj)) {
+        return obj.map(item => deepClone(item));
+    }
+    
+    const cloned = {};
+    for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            cloned[key] = deepClone(obj[key]);
+        }
+    }
+    return cloned;
+}
+
+function validateStateUpdate(updates) {
+    if (!updates || typeof updates !== 'object') {
+        throw new Error('Invalid state update: must be an object');
+    }
+    
+    if ('currentClause' in updates && updates.currentClause !== null) {
+        const clauseNum = parseInt(updates.currentClause);
+        if (isNaN(clauseNum) || clauseNum < 1 || clauseNum > 10) {
+            throw new Error('Invalid currentClause value');
+        }
+    }
+    
+    return true;
+}
+
+export const getAppState = () => deepClone(appState);
+
+export const updateAppState = (updates) => {
+    try {
+        validateStateUpdate(updates);
+        appState = { ...appState, ...updates };
+        return getAppState();
+    } catch (error) {
+        console.error('Invalid state update:', error);
+        return getAppState();
+    }
+};
+
+export const resetAppState = () => {
+    appState = deepClone(DEFAULT_STATE);
+    return getAppState();
+};
+
+export const getClauseData = (clauseNumber) => {
+    const num = parseInt(clauseNumber);
+    if (isNaN(num) || num < 1 || num > 10) {
+        return null;
+    }
+    return deepClone(getAppState().clausesData[num] || null);
+};
